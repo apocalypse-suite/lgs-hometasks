be0bd573845454c2e74d8d5ed8020f6c1ffcaf60
apocalypse-suite <mitchenko.catherine@gmail.com>
2020-09-14 13:48:07 +0300

    switch between forms

--- localstorage/script.js
+++ localstorage/script.js
@@ -1,19 +1,23 @@
-let formTwo = document.forms[0];
+let signInFormDiv = document.getElementById('signInForm');
+let signUpFormDiv = document.getElementById('signUpForm');
+let signInForm = document.forms[0];
+let signUpForm = document.forms[1];
+signUpFormDiv.style.display = 'none';
 
 function termsCheck() {
-    if (formTwo.terms.checked) {
-        formTwo.submitBtn.disabled = false;
+    if (signUpForm.terms.checked) {
+        signUpForm.submitBtn.disabled = false;
     } else {
-        formTwo.submitBtn.disabled = true;
+        signUpForm.submitBtn.disabled = true;
     }
 }
 
-formTwo.submitBtn.onclick = function (event) {
-    document.getElementById('signInForm').style.display = "none";
+signUpForm.submitBtn.onclick = function (event) {
+    signInForm.style.display = "none";
     document.getElementById('profile').style.display = "block";
-    document.getElementById('userName').innerText = `${formTwo.firstName.value}` + ` ` + `${formTwo.lastName.value}`;
-    document.getElementById('email').innerText = `${formTwo.myMail.value}`;
-    document.getElementById('position').innerText = `${formTwo.position.value}`;
+    document.getElementById('userName').innerText = `${signUpForm.firstName.value}` + ` ` + `${signUpForm.lastName.value}`;
+    document.getElementById('email').innerText = `${signUpForm.myMail.value}`;
+    document.getElementById('position').innerText = `${signUpForm.position.value}`;
 }
 
 function checkSex(type) {
@@ -27,7 +31,24 @@ function checkSex(type) {
 
 let signOutBtn = document.getElementById('signOut');
 signOutBtn.addEventListener('click', function () {
-    formTwo.reset();
+    signUpForm.reset();
     document.getElementById('profile').style.display = "none";
-    document.getElementById('signInForm').style.display = "block";
+    signInForm.style.display = "block";
+})
+
+let signUpLink = document.getElementsByClassName('registerNow')[0];
+signUpLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    signInFormDiv.style.display = 'none';
+    signUpLink.style.display = 'none';
+    signInLink.style.display = 'block';
+    signUpFormDiv.style.display = 'block';
+})
+let signInLink = document.getElementsByClassName('loginNow')[0];
+signInLink.addEventListener('click', function (e) {
+    e.preventDefault();
+    signUpFormDiv.style.display = 'none';
+    signInLink.style.display = 'none';
+    signUpLink.style.display = 'block';
+    signInFormDiv.style.display = 'block';
 })

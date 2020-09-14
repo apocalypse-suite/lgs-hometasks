9dcb8b9686eb497883f4a5e506779b560617e7ef
apocalypse-suite <mitchenko.catherine@gmail.com>
2020-09-14 12:41:49 +0300

    minor change

--- post-comments/script.js
+++ post-comments/script.js
@@ -1,5 +1,6 @@
 let comments = [];
 let posts = [];
+let userPictures = ['https://likewise-stage.azureedge.net/uploads/3eb6cf23-895b-45e9-b92c-5fb1b457dd04/bill-gates-profile-pic.jpg', 'https://i.ytimg.com/vi/KUwROhuhZ-Q/maxresdefault.jpg', 'https://pbs.twimg.com/media/DAjkAE_XoAM_SKF.jpg', 'https://1.bp.blogspot.com/-QaeKO6CXz8U/TbqlGsVGsxI/AAAAAAAADyY/0FFeXF_nN3A/s1600/Dame%2BEdna%2B1.jpg', 'https://wallpaperaccess.com/full/2090923.jpg', 'https://64.media.tumblr.com/036c3491ca544f1893a284b5ac746fd7/tumblr_pvxkrjVzhA1xb21v7o2_500.jpg']
 
 async function getComments() {
     try {
@@ -65,7 +66,7 @@ async function main() {
 main();
 
 function drawBlocks() {
-    let colors = ['#03071e', '#370617', '#6a040f', '#9d0208', '#d00000', '#dc2f02', '#e85d04', '#f48c06', '#faa307'];
+    // let colors = ['#03071e', '#370617', '#6a040f', '#9d0208', '#d00000', '#dc2f02', '#e85d04', '#f48c06', '#faa307'];
 
     posts.forEach(post => {
         let container = document.querySelector('.container');
@@ -85,8 +86,9 @@ function drawBlocks() {
             userComment.innerHTML = `<h4>${comment.name}</h4><h6><a href="#" style="color:black;">${comment.email}</a></h6> <p>${comment.body}</p>`;
         })
     });
-    let postTitles = Array.prototype.slice.call(document.querySelectorAll('.postTitle'));
-    postTitles.forEach(elem => {
-        elem.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];;
-    })
+    // let postTitles = Array.prototype.slice.call(document.querySelectorAll('.postTitle'));
+    // postTitles.forEach(elem => {
+    //     elem.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];;
+    // })
+
 }
